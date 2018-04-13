9179bf630cfd9ee3ef42ae677e6cef33fda7db2a
crh2108@gmail.com <crh2108@gmail.com>
2018-04-13 15:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {prop: 1}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, recipes, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
